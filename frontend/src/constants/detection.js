de6e6df4e3d2ed9ea9551b3f0b249611ac4bf208
kumaravel <kumaravelb9342@gmail.com>
2026-05-16 21:36:37 +0530

    Remove all files from repository

--- frontend/src/constants/detection.js
+++
@@ -1,5 +0,0 @@
-﻿export const ALLOWED_EXTENSIONS = new Set(["mp4", "avi", "mov", "mkv", "webm"]);
-export const MAX_SIZE_MB = 100;
-
-export const ACCEPTED_VIDEO_TYPES =
-  "video/mp4,video/avi,video/quicktime,video/x-matroska,video/webm";
